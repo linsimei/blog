40847155577967b27f602ae2c1e95bf5cb99d649
huangzl <huangzl@tsta.com.cn>
2017-08-20 22:42:36 +0800

    auth

---
+++ middlewares/restauth.js
@@ -0,0 +1,12 @@
+function isAuthenticated(req, res, next) {
+    
+        // do any checks you want to in here
+    
+        // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+        // you can do this however you want with whatever variables you set up
+        if (req.user.authenticated)
+            return next();
+    
+        // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
+        res.send("no auth");
+    }
--- routes/web.js
+++ routes/web.js
@@ -1,27 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var auth = require("../middlewares/auth");
+
 
 var webs = require("../models/webs");
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    webs.find({},function(err,webs){
+    webs.find({}, function (err, webs) {
         res.json(webs);
     });
-    
+
 });
 
-router.get("/add", function (req, res, next) {
-    // var title = req.body.title;
-    // var content = req.body.content;
-    var title = "title";
-    var content = "content";
-    var new_web = new webs({title:title,content:content,createDate:new Date()});
-    new_web.save(function (err, new_web) {
-        if (err) return console.error(err);
-        res.send("new_web");
+router.get("/:id",auth, function (req, res, next) {
+    var id = req.params.id;
+    webs.findById(id, function (err, item) {
+        res.json(item);
     });
 
-
 })
+
+router.delete("/:id", function (req, res, next) {
+    webs.remove({ _id: req.params.id }, function (err) {
+        res.json(true);
+    });
+});
+
+router.post("/add", function (req, res, next) {
+    var title = req.body.title;
+    var content = req.body.content;
+    var new_web = new webs({ title: title, content: content, createDate: new Date() });
+    new_web.save(function (err, new_web) {
+        if (err) return console.error(err);
+        res.json(new_web);
+    });
+})
+
 
 module.exports = router;
