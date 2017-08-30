6b62c4470a7f1dbc201d991f80ff9f7b618d50de
linsimei <772094242@qq.com>
2017-08-30 23:02:10 +0800

    关于我

--- public/js/manage.js
+++ public/js/manage.js
@@ -3,7 +3,16 @@ $(function () {
     $.get("/web", function (data) {
         var html = "";
         data.forEach(function (item) {
-            html += '<div class="manage_blog clearfix"> <div class="manage_cover float_left"><img src="'+item.cover+'" alt="博客封面"/></div><div class="manage_right float_left"><div class="manage_title"><a href="">' + item.title + '</a></div><div class="manage_items"><a class="content" href="/web/details?id='+item._id+'">查看</a><a class="edit" href="/web/edit/' + item._id + '">修改</a> <a class="delete" href="/web/delete/' + item._id + '">删除</a></div></div></div>';
+            html += '<div class="manage_blog clearfix"> '
+            +'<div class="manage_cover float_left"><img src="'+item.cover+'" alt="博客封面"/></div>'
+            +'<div class="manage_right float_left">'
+            +'<div class="manage_title"><a href="">' + item.title + '</a></div>'
+            +'<div class="manage_items">'
+            +'<a class="content" href="/web/details?id='+item._id+'">查看</a>'
+            +'<a class="edit" href="/web/edit/' + item._id + '">修改</a>'
+            +'<a class="delete" href="/web/delete/' + item._id + '">删除</a>'
+            + '<a class="edit" href="/web/edit/' + item._id + '">'+item.recommond?'已推荐':'推荐' +'</a>'
+            +'</div></div></div>';
         }, this);
         $('#manage_wrapper').html(html);
         //删除确认
