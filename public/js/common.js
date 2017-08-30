a23e3531ae336dbf47af3304ce40713c870d177e
linsimei <772094242@qq.com>
2017-08-30 17:47:30 +0800

    index 页面优化

--- public/js/common.js
+++ public/js/common.js
@@ -11,3 +11,20 @@ $(function () {
         }
     });
 });
+
+//date format
+
+function padLeft(n, str) {
+    return Array(n - String(this).length + 1).join(str || '0') + this;
+}
+
+if (!Date.prototype.format) {
+    Date.prototype.format = function(f) {
+        f = f.replace(/yyyy/g, this.getFullYear());
+        f = f.replace(/MM/g, this.getMonth() + 1);
+        f = f.replace(/dd/g, this.getDate());
+        f = f.replace(/d\*/g, this.getDate());
+        return f;
+    };
+}
+// module.exports = Date;
--- public/js/details.js
+++ public/js/details.js
@@ -3,10 +3,9 @@ $(function () {
     var url = new URL(window.location.href)
     var id = url.searchParams.get("id");
     $.get("/web/item/"+id, function (data) {
-       
         $(".content_title").html(data.title);
-        $(".sumit_time").html(data.createDate);
-        $(".blogs_content").html(data.content);
+        var time= new Date(data.createDate).format('yyyy-MM-dd')
+        $(".sumit_time").html(time);
         $(".blogs_content").html(data.content);
         $(".views_num").html(data.visit);
         
