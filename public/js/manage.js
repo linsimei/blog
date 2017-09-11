4d529e053e42eabf12453ae23f42c70a31dbe758
linsimei <772094242@qq.com>
2017-09-11 23:41:07 +0800

    manage.js vue版本

--- public/js/manage.js
+++ public/js/manage.js
@@ -1,28 +1,46 @@
-$(function () {
-    //获取博客列表
-    $.get("/web", function (data) {
-        var html = "";
-        data.forEach(function (item) {
-            html += '<div class="manage_blog clearfix"> '
-            +'<div class="manage_cover float_left"><img src="'+item.cover+'" alt="博客封面"/></div>'
-            +'<div class="manage_right float_left">'
-            +'<div class="manage_title"><a href="/web/edit/' + item._id + '">' + item.title + '</a></div>'
-            +'<div class="manage_items">'
-            +'<a class="content" href="/web/details?id='+item._id+'">查看</a>'
-            +'<a class="edit" href="/web/edit/' + item._id + '">修改</a>'
-            +'<a class="delete" href="/web/delete/' + item._id + '">删除</a>'
-            + '<a class="recommend" href="/web/recommend/' + item._id + '">'+(item.recommend?'取消推荐':'推荐') +'</a>'
-            +'</div></div></div>';
-        }, this);
-        $('#manage_wrapper').html(html);
-        //删除确认
-        $('.delete').on('click', function () {
-            return confirm('确认删除博客?');
-        });
+// $(function () {
+//     //获取博客列表
+//     $.get("/web", function (data) {
+//         var html = "";
+//         data.forEach(function (item) {
+//             html += '<div class="manage_blog clearfix"> '
+//             +'<div class="manage_cover float_left"><img src="'+item.cover+'" alt="博客封面"/></div>'
+//             +'<div class="manage_right float_left">'
+//             +'<div class="manage_title"><a href="/web/edit/' + item._id + '">' + item.title + '</a></div>'
+//             +'<div class="manage_items">'
+//             +'<a class="content" href="/web/details?id='+item._id+'">查看</a>'
+//             +'<a class="edit" href="/web/edit/' + item._id + '">修改</a>'
+//             +'<a class="delete" href="/web/delete/' + item._id + '">删除</a>'
+//             + '<a class="recommend" href="/web/recommend/' + item._id + '">'+(item.recommend?'取消推荐':'推荐') +'</a>'
+//             +'</div></div></div>';
+//         }, this);
+//         $('#manage_wrapper').html(html);
+//         //删除确认
+//         $('.delete').on('click', function () {
+//             return confirm('确认删除博客?');
+//         });
 
 
+//     })
+
+
+
+// })
+window.onload = function () {
+    var vue = new Vue({
+        el: "#manage_wrapper",
+        data: {
+            manage_blogs: []
+        },
+        mounted: function () {
+            this.$nextTick(function () {
+                this.$http.get('/web').then(response => {
+                    this.manage_blogs = response.body;
+                });
+            })
+        },
+        methods: {
+          
+        }
     })
-    
-
-
-})
+}
