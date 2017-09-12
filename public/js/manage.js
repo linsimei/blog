5a4ff2ebc5db6409144e6b53a267cbf248a9f186
linsimei <772094242@qq.com>
2017-09-13 00:14:14 +0800

    blog manage

--- public/js/manage.js
+++ public/js/manage.js
@@ -1,31 +1,4 @@
-// $(function () {
-//     //获取博客列表
-//     $.get("/web", function (data) {
-//         var html = "";
-//         data.forEach(function (item) {
-//             html += '<div class="manage_blog clearfix"> '
-//             +'<div class="manage_cover float_left"><img src="'+item.cover+'" alt="博客封面"/></div>'
-//             +'<div class="manage_right float_left">'
-//             +'<div class="manage_title"><a href="/web/edit/' + item._id + '">' + item.title + '</a></div>'
-//             +'<div class="manage_items">'
-//             +'<a class="content" href="/web/details?id='+item._id+'">查看</a>'
-//             +'<a class="edit" href="/web/edit/' + item._id + '">修改</a>'
-//             +'<a class="delete" href="/web/delete/' + item._id + '">删除</a>'
-//             + '<a class="recommend" href="/web/recommend/' + item._id + '">'+(item.recommend?'取消推荐':'推荐') +'</a>'
-//             +'</div></div></div>';
-//         }, this);
-//         $('#manage_wrapper').html(html);
-//         //删除确认
-//         $('.delete').on('click', function () {
-//             return confirm('确认删除博客?');
-//         });
 
-
-//     })
-
-
-
-// })
 window.onload = function () {
     var vue = new Vue({
         el: "#manage_wrapper",
@@ -40,7 +13,18 @@ window.onload = function () {
             })
         },
         methods: {
-          
+            delete_blog: function (event) {
+                var id = event.currentTarget.getAttribute("data-id");
+                var result = confirm("确认删除?");
+                if(result){
+                    vue.$http.get("/web/delete/"+id).then(function(res){
+                        alert("删除成功！")
+                        window.location.href="/manage"
+                    })
+                }
+            }
+
+
         }
     })
 }
--- public/js/web_create.js
+++ public/js/web_create.js
@@ -1,52 +1,65 @@
-$(function(){
-    $("#pre_img").click(function(){
-        $("#upload_file").click();
-    });
-    $("#upload_file").change(function(){
-        //jq dom 对象 转js dom对象
-        var file = $(this)[0].files[0];
-        var formData = new FormData();
-        formData.append('file', file);
-        
-        $.ajax({
-               url : '/files/upload',
-               type : 'POST',
-               data : formData,
-               processData: false,  // tell jQuery not to process the data
-               contentType: false,  // tell jQuery not to set contentType
-               success : function(data) {
-                   $("#pre_img").attr("src",data);
-                   $("#cover").val(data);
-               }
-        });
+window.onload = function () {
+    var vue = new Vue({
+        el: "#create_form",
+        data: {
+            blog: {
+                title: '',
+                cover: '',
+                content: ''
+            }
+        },
+        methods: {
+            upload_file: function () {
+                $("#upload_file").click();
+            },
+            upload_img: function (event) {
+                var file = event.target.files[0];
+                var formData = new FormData();
+                formData.append('file', file);
+
+                $.ajax({
+                    url: '/files/upload',
+                    type: 'POST',
+                    data: formData,
+                    processData: false,  // tell jQuery not to process the data
+                    contentType: false,  // tell jQuery not to set contentType
+                    success: function (data) {
+                        $("#pre_img").attr("src", data);
+                        vue.blog.cover = data;
+
+
+                    }
+                });
+            },
+            submit: function () {
+
+                this.$http.post("/web/add", vue.blog).then(response => {
+                    response.body;
+                    alert("发表成功！");
+                    window.location.href = "/manage";
+
+                }
+                )
+            }
+
+        },
+        mounted: function () {
+            var E = window.wangEditor;
+            var editor = new E('#wangeditor');
+            // 自定义菜单配置
+            editor.customConfig.menus = [
+                'head',
+                'bold',
+                'italic',
+                'underline'
+            ]
+            editor.customConfig.onchange = function (html) {
+                // html 即变化之后的内容
+                //$("#content").val(html);
+                vue.blog.content = html;
+            }
+            // 或者 var editor = new E( document.getElementById('#editor') )
+            editor.create()
+        }
     })
-
-//ajax表单提交
-    $("#submit").click(function(){
-         var formdata  = $("#create_form").serialize();
-         $.post("/web/add",formdata,function(data){
-             alert("发表成功！");
-             window.location.href = "/manage";
-         })
-
-    });
-
-    //wangEditor
-    var E = window.wangEditor;
-    var editor = new E('#wangeditor');
-    // 自定义菜单配置
-    editor.customConfig.menus = [
-        'head',
-        'bold',
-        'italic',
-        'underline'
-    ]
-    editor.customConfig.onchange = function (html) {
-        // html 即变化之后的内容
-        $("#content").val(html);
-    }
-    // 或者 var editor = new E( document.getElementById('#editor') )
-    editor.create()
-
-
-})
+}
